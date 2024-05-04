32b276d2cf7fe9e80d23295d9d442f6ce94bd030
sohail <sksohail.swaraj@gmail.com>
2024-05-05 05:04:26 +0530

    cod removed

--- src/components/CheckoutPage.jsx
+++ src/components/CheckoutPage.jsx
@@ -79,7 +79,7 @@ const CheckoutPage = () => {
       if (itemId) {
         try {
           const res = await axios.post(
-            "https://atom-creations-backend.vercel.app/api/appuser/placeorder",
+            "http://localhost:8080/api/appuser/placeorder",
             {
               address,
               email,
@@ -121,7 +121,7 @@ const CheckoutPage = () => {
         }));
         try {
           const res = await axios.post(
-            "https://atom-creations-backend.vercel.app/api/appuser/placeorder",
+            "http://localhost:8080/api/appuser/placeorder",
             {
               address,
               city,
@@ -193,7 +193,7 @@ const CheckoutPage = () => {
       if (itemId) {
         try {
           const res = await axios.get(
-            `https://atom-creations-backend.vercel.app/api/products/${itemId}`
+            `http://localhost:8080/api/products/${itemId}`
           );
 
           setItem(res.data);
@@ -208,7 +208,7 @@ const CheckoutPage = () => {
           const YOUR_TOKEN = localStorage.getItem("token");
           if (YOUR_TOKEN) {
             const myCart = await axios.get(
-              "https://atom-creations-backend.vercel.app/api/appuser/getcartitem",
+              "http://localhost:8080/api/appuser/getcartitem",
               {
                 headers: {
                   Authorization: `Bearer ${YOUR_TOKEN}`, // Assuming the token is a bearer token
@@ -507,22 +507,7 @@ const CheckoutPage = () => {
                         </span> */}
                       </label>
                     </div>
-                    <div className="flex items-center">
-                      <input
-                        type="radio"
-                        id="cod"
-                        name="paymentMethod"
-                        value="cod"
-                        checked={formData.paymentMethod === "cod"}
-                        onChange={handleChange}
-                        className="mr-2"
-                      />
-                      <label htmlFor="cod" className="text-lg">
-                        Cash on Delivery (COD)
-                      </label>
-                    </div>
                   </div>
-
                   <div className="mb-4">
                     <p className="text-xl font-bold">
                       Total Price: ₹ {TotalPrice}
