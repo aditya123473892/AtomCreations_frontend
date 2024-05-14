53d7adc44645f252a432c507511a9acf0d636f3a
sohail <sksohail.swaraj@gmail.com>
2024-05-14 23:34:33 +0530

    slider with hover effect

--- src/components/Home/FeaturedCollection/Collections.jsx
+++ src/components/Home/FeaturedCollection/Collections.jsx
@@ -3,7 +3,7 @@ import CollectionSlider from "./CollectionSlider";
 const Collections = () => {
   return (
     <section className="gap-10 text-black">
-      <h1 className="p-12 text-6xl text-center font-heading text">Featured Designs</h1>
+     <h1 className="p-12 text-5xl text-center font-arial-semibold text-gray-600">NEW IN</h1>
       <CollectionSlider/>
 
     
