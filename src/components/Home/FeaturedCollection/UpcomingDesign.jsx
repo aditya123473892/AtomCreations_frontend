eaf3b9d4aede224b3201ef84a5bb6f78ee2ec0ed
KritikaBindal07 <kirtibindalkb770@gmail.com>
2024-05-17 14:14:56 +0530

    typography

--- src/components/Home/FeaturedCollection/UpcomingDesign.jsx
+++ src/components/Home/FeaturedCollection/UpcomingDesign.jsx
@@ -4,7 +4,7 @@ const UpcomingDesign = () => {
   return (
     <section className="gap-10 text-black">
 
-      <h1 className="p-12 text-6xl text-center font-heading text">Upcoming Designs</h1>
+      <h1 className="py-12 text-5xl md:text-6xl text-center font-heading text">Upcoming Designs</h1>
 
       <CollectionSlider/>
 
