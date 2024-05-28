0436c297f5084450e06c7dfd8250135f9848df4f
aditya123473892 <adityathakur6199@gmail.com>
2024-05-28 23:53:08 +0530

    renew

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -63,7 +63,7 @@ const Orders = () => {
       const token = localStorage.getItem("token");
       try {
         const res = await axios.get(
-          "https://atom-creations-backend-git-main-adityas-projects-a14514f1.vercel.app/api/appuser/getuserorders",
+          "https://atom-creations-backend.vercel.app/api/appuser/getuserorders",
           {
             headers: {
               Authorization: `Bearer ${token}`,
