d76573c19a2c206de8d088e2b7768562a2b23c70
aditya123473892 <adityathakur6199@gmail.com>
2024-09-09 15:08:28 +0530

    main

--- src/components/ContextProvider/AuthContext.jsx
+++ src/components/ContextProvider/AuthContext.jsx
@@ -12,7 +12,7 @@ export const AuthProvider = ({ children }) => {
         const YOUR_TOKEN = localStorage.getItem("token");
         if (YOUR_TOKEN) {
           const user = await axios.get(
-            "https://atom-creations-backend-rc06amtad-adityas-projects-a14514f1.vercel.app/api/user/fetchuser",
+            "https://atom-creations-backend.vercel.app/api/user/fetchuser",
             {
               headers: {
                 Authorization: `Bearer ${YOUR_TOKEN}`,
